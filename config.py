2a309e44523db9339a8297f23054cf727d0343bd
zxc_322 <kvasoliaeww@gmail.com>
2021-12-16 23:18:14 +0200

    chek

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '2118483051:AAHrI-Fj4FuzTWO75ecF7GxODqjwmYkIAJo'
+app_url = 'https://test-022.herokuapp.com/' + token
---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import telebot
+import logging
+from flask import Flask, request
+from config import *
+
+bot = telebot.TeleBot(token)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    username = message.from_user.ussername
+    bot.reply_to(message, f'darova {username}')
+
+
+@server.route(f'/{token}', methods=['POST'])
+def redirect_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=app_url)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
