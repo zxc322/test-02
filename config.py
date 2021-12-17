1031c58a1cb452b66a6759e67d096f2b893feb1a
zxc_322 <91989456+zxc322@users.noreply.github.com>
2021-12-17 02:01:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-token = '2118483051:AAHrI-Fj4FuzTWO75ecF7GxODqjwmYkIAJo'
+token = '5053164398:AAFAfN0xnv93dXzyPoDKbF7mCeum7K4COLs'
 app_url = 'https://test-022.herokuapp.com/' + token
+ngrok_url = 'https://765d-93-75-106-174.ngrok.io/' +token
+COINMARKETCAP_API_KEY = '2aae45ae-7242-4437-9495-1544463903b3'
+TELEGRAM_URL = 'https://api.telegram.org/bot'
+COINMARKETCAP_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
